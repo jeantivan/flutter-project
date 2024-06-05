b1951107c37e47f0f80871e5376ae3591065c9a4
jeantivan <jptivan53@gmail.com>
2024-06-05 20:01:58 +0200

    UPTADE: Preparando para crear la vista

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_flutter/widget/layout.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Layout(
-      child: Center(
-        child: Text("Settings page"),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return const Center(
+      child: Text("Settings page"),
     );
   }
 }

9cd6ea3809b9a1f1a6fd46292fa0a4025a09f093
jeantivan <jptivan53@gmail.com>
2024-06-06 20:50:11 +0200

    CHANGE: Trabajando en la vista para Crear transacciones

--- lib/pages/create_transaction.dart
+++ lib/pages/create_transaction.dart
@@ -1,20 +1,131 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class CreateTransaction extends StatelessWidget {
+enum TransactionType {
+  income,
+  expense,
+}
+
+class MontoView extends StatefulWidget {
+  const MontoView({super.key});
+
+  @override
+  State<MontoView> createState() => _MontoViewState();
+}
+
+class _MontoViewState extends State<MontoView> {
+  TransactionType transactionType = TransactionType.expense;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      children: [
+        const SizedBox(
+          height: 88,
+        ),
+        SegmentedButton(
+          segments: const [
+            ButtonSegment(
+                value: TransactionType.expense, label: Text("Gastos")),
+            ButtonSegment(
+                value: TransactionType.income, label: Text("Ingresos"))
+          ],
+          selected: <TransactionType>{transactionType},
+          onSelectionChanged: (newSelection) {
+            setState(() {
+              transactionType = newSelection.first;
+            });
+          },
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Column(
+          children: [
+            Text(
+              "12123,12",
+              style: theme.textTheme.displayMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 126,
+              height: 6,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: theme.colorScheme.primary),
+            )
+          ],
+        ),
+        const Spacer(),
+        Row(
+          children: [
+            Expanded(
+                child: FilledButton(
+                    onPressed: () {}, child: const Text("Siguiente"))),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        )
+      ],
+    );
+  }
+}
+
+class CreateTransaction extends StatefulWidget {
   const CreateTransaction({super.key});
+
+  @override
+  State<CreateTransaction> createState() => _CreateTransactionState();
+}
+
+class _CreateTransactionState extends State<CreateTransaction>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+  final int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Transascciones"),
-      ),
-      body: Center(
-        child: Text(
-          "Crear transaccion",
-          style: Theme.of(context).textTheme.displayMedium,
+        appBar: AppBar(
+          title: const Text("Crear transacción"),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(child: Text("Monto")),
+              Tab(
+                child: Text("Info"),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            MontoView(),
+            Center(
+              child: Text(
+                "Paso 2",
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ),
+          ],
+        ));
   }
 }
